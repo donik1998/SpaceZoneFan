31ddf0fe7f09e3d7e76055e89252a758183b8250
donik1998 <donikmurodkulov98@gmail.com>
2020-09-02 17:03:22 +0500

    initial commit

---
+++ android/app/src/main/kotlin/com/example/spacefanzone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.spacefanzone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+
+  ),);
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
