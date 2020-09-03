5b47cf6f8f24ce5a1e0670512d4503da53ecfc6f
donik1998 <donikmurodkulov98@gmail.com>
2020-09-03 16:00:56 +0500

    basic UI implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,54 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: HomePage(),
-
-  ),);
+  runApp(
+    MaterialApp(
+      home: HomePage(),
+    ),
+  );
 }
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('NASA API features'),
+          bottom: TabBar(
+            indicatorPadding: EdgeInsets.all(5.0),
+            indicatorWeight: 2.0,
+            labelPadding: EdgeInsets.all(5.0),
+            indicatorSize: TabBarIndicatorSize.tab,
+            indicatorColor: Colors.blue.shade200,
+            tabs: [
+              Text('Earth'),
+              Text('Pic of the day'),
+            ],
+          ),
+        ),
+        body: Container(
+          child: TabBarView(
+            children: [
+              Center(
+                child: Container(
+                  color: Colors.red,
+                  height: 100.0,
+                  width: 100.0,
+                ),
+              ),
+              Center(
+                child: Container(
+                  color: Colors.red,
+                  height: 100.0,
+                  width: 100.0,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
