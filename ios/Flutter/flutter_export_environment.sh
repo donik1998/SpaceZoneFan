25aa267ee4ccd65c7d4fbe44b975cb1f7a61c1f8
doniyormurodkulov <donikmurodkulov98@gmail.com>
2022-04-19 12:22:26 +0500

    message

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/doniyormurodkulov/Documents/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/doniyormurodkulov/StudioProjects/space_fun_zone"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/doniyormurodkulov/StudioProjects/space_fun_zone/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/doniyormurodkulov/StudioProjects/space_fun_zone/.dart_tool/package_config.json"
